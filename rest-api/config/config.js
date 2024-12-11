88ca72866cb5238270e7a6139fe04c4913349992
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2024-12-11 22:46:21 +0100

    mongo db connection string edit , readme edit

--- rest-api/config/config.js
+++ rest-api/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3100,
-        dbURL: 'mongodb://localhost:27017/phonestore',
+        dbURL: 'mongodb://127.0.0.1:27017/phonestore',
         origin: ['http://localhost:5555', 'http://localhost:4300']
     },
     production: {
